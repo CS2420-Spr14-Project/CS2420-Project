96ea859be437b3eccf74a683d4013308562f25ab
Skyler Cowley <skyler.cowley@gmail.com>
2014-03-25 10:10:53 -0600

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,11 @@
+public class SearchEngine {
+    //this is a class to create a search engine
+
+    //we need to parse files
+
+    //and we need to store the data
+
+    //we then need to search the data
+
+    //and we need to desplay the results
+}
