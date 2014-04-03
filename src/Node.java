ccd9cbf3680bfba1d56fad7adb49e617f4ed228d
Skyler Cowley <skyler.cowley@gmail.com>
2014-04-03 08:33:54 -0600

    Search engine with working hashes and basic GUI

--- src/Node.java
+++ src/Node.java
@@ -1,47 +1,45 @@
 /**
  * Created on 3/26/14.
  */
-public class Node{
+public class Node {
     String word;
     Node next;
     Node docs;
 
-    Node(){
+    Node() {
         word = null;
         next = null;
         docs = null;
     }
 
-    Node(String wordIn){
+    Node(String wordIn) {
         word = wordIn;
         next = null;
         docs = null;
     }
 
-    Node(String wordIn, String docIn){
+    Node(String wordIn, String docIn) {
         word = wordIn;
         next = null;
         docs = new Node(docIn);
     }
 
-    void insert(Node inNode){
+    void insert(Node inNode) {
 
-        if(this.word.compareTo(inNode.word)==0 && inNode.docs != null){
+        if (this.word.compareTo(inNode.word) == 0 && inNode.docs != null) {
             Node docIn = new Node(inNode.docs.word);
             this.docs.insert(docIn);
-        }
-        else if(this.word.compareTo(inNode.word)==0 && inNode.docs == null){
+        } else if (this.word.compareTo(inNode.word) == 0 && inNode.docs == null) {
 
-        }
-        else{
-            if(this.next == null)
-               this.next = inNode;
+        } else {
+            if (this.next == null)
+                this.next = inNode;
             else
-               this.next.insert(inNode);
+                this.next.insert(inNode);
         }
     }
 
-    String printDocs(){
-        return this.next == null ? this.word: this.word.concat("\n"+this.next.printDocs());
+    String printDocs() {
+        return this.next == null ? this.word : this.word.concat("\n" + this.next.printDocs());
     }
 }
--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -17,8 +17,8 @@ public class SearchEngine {
     //final int stopWordsSize = 54;
     final int stopWordsSize = 28;
     //String [] words = new String[SIZE];
-    Node [] words = new Node[SIZE];
-    Node [] stopWords = new Node[stopWordsSize];
+    Node[] words = new Node[SIZE];
+    Node[] stopWords = new Node[stopWordsSize];
 
     //constructor
     public SearchEngine() throws FileNotFoundException {
@@ -34,14 +34,14 @@ public class SearchEngine {
         stopWord();
 
         //creating array to hash words into
-        for(int i = 0; i < SIZE; i++)
+        for (int i = 0; i < SIZE; i++)
             words[i] = new Node();
 
         //scans through each document and stores the words in an array
-        for(int i = 1; i<51; i++){
+        for (int i = 1; i < 51; i++) {
 
             //generating file name
-            fileName = i<10?"cranfield000":"cranfield00";
+            fileName = i < 10 ? "cranfield000" : "cranfield00";
 
             fileNum = Integer.toString(i);
             filePath = ".\\Project\\documents\\";
@@ -52,40 +52,38 @@ public class SearchEngine {
             //reading in from the files
             Scanner inFile = new Scanner(new FileReader(filePath));
 
-            while(inFile.hasNext()){
+            while (inFile.hasNext()) {
                 String wordIn = inFile.next();
 
                 //skip <> tags and .
-                if(!wordIn.startsWith("<") && !wordIn.startsWith(".")){
+                if (!wordIn.startsWith("<") && !wordIn.startsWith(".")) {
                     //remove commas
-                    if(wordIn.endsWith(","))
+                    if (wordIn.endsWith(","))
                         wordIn = wordIn.replace(",", "");
                     //check to see if it is stop word
 
 
                     //if(!stopCheck(wordIn)){
-                    if(!stopCheck(wordIn, stopWords[stopHash(wordIn)%stopWordsSize])){
+                    if (!stopCheck(wordIn, stopWords[stopHash(wordIn) % stopWordsSize])) {
 
                         //insert into hash table
                         Node toInsert = new Node(wordIn, fileName);
-                        System.out.println(wordIn +", "+ fileName);
+                        System.out.println(wordIn + ", " + fileName);
                         int value = mainHash(wordIn);
 
-                        if(words[value%SIZE].word == null){
-                            words[value%SIZE].word = toInsert.word;
-                            words[value%SIZE].docs = toInsert.docs;
-                        }
-                        else if(words[value%SIZE].word.compareTo(toInsert.word)==0){
+                        if (words[value % SIZE].word == null) {
+                            words[value % SIZE].word = toInsert.word;
+                            words[value % SIZE].docs = toInsert.docs;
+                        } else if (words[value % SIZE].word.compareTo(toInsert.word) == 0) {
                             Node docIn = new Node(toInsert.docs.word);
-                            if(words[value%SIZE].docs == null)
-                                words[value%SIZE].docs = toInsert;
+                            if (words[value % SIZE].docs == null)
+                                words[value % SIZE].docs = toInsert;
                             else
-                                words[value%SIZE].docs.insert(docIn);
-                        }
-                        else if(words[value%SIZE].next == null)
-                            words[value%SIZE].next = toInsert;
+                                words[value % SIZE].docs.insert(docIn);
+                        } else if (words[value % SIZE].next == null)
+                            words[value % SIZE].next = toInsert;
                         else
-                            words[value%SIZE].next.insert(toInsert);
+                            words[value % SIZE].next.insert(toInsert);
                     }
                 }
             }
@@ -93,12 +91,12 @@ public class SearchEngine {
 
         //prints out the array of stored words
         System.out.println("Array of stored words:");
-        for(int i = 0; i < SIZE; i++){
-            System.out.println(i + " "+words[i].word);
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println(i + " " + words[i].word);
         }
         //prints out the array of stored words
         System.out.println("Array of stop words(withought linked lists):");
-        for(int j = 0; j < stopWordsSize; j++){
+        for (int j = 0; j < stopWordsSize; j++) {
             System.out.println(stopWords[j].word);
         }
     }
@@ -108,10 +106,10 @@ public class SearchEngine {
 
         Scanner inFile = new Scanner(new FileReader(".\\Project\\stopwords.txt"));
 
-        for(int i = 0; i < stopWordsSize; i++)
+        for (int i = 0; i < stopWordsSize; i++)
             stopWords[i] = new Node();
 
-        while(inFile.hasNext()){
+        while (inFile.hasNext()) {
             String wordIn = inFile.next();
 
             //convert word to ASCII value
@@ -119,79 +117,77 @@ public class SearchEngine {
 
             Node inNode = new Node(wordIn);
 
-            if(stopWords[value%stopWordsSize].word == null)
-                stopWords[value%stopWordsSize].word = inNode.word;
-            else if(stopWords[value%stopWordsSize].next == null)
-                stopWords[value%stopWordsSize].next = inNode;
+            if (stopWords[value % stopWordsSize].word == null)
+                stopWords[value % stopWordsSize].word = inNode.word;
+            else if (stopWords[value % stopWordsSize].next == null)
+                stopWords[value % stopWordsSize].next = inNode;
             else
-                stopWords[value%stopWordsSize].next.insert(inNode);
+                stopWords[value % stopWordsSize].next.insert(inNode);
         }
     }
 
     //recursively checks a given string against the stopWords hash table
     //if the word is in the stopWords array it returns true
-    boolean stopCheck(String inString, Node inNode){
+    boolean stopCheck(String inString, Node inNode) {
 
-        if(inNode.word == null)
+        if (inNode.word == null)
             return false;
-        else if(inString.compareTo(inNode.word) == 0)
+        else if (inString.compareTo(inNode.word) == 0)
             return true;
-        else if(inNode.next == null)
+        else if (inNode.next == null)
             return false;
         else
             return stopCheck(inString, inNode.next);
     }
 
 
-    void search(){
+    void search() {
 
-    String input = JOptionPane.showInputDialog(null, "What would you like to search for?", "Search");
+        String input = JOptionPane.showInputDialog(null, "What would you like to search for?", "Search");
         int value = mainHash(input);
-    String output;
-        if(words[value%SIZE].word == null)
+        String output;
+        if (words[value % SIZE].word == null)
             output = "Word not found.";
-        else if(words[value%SIZE].word.compareTo(input) == 0){
-            output = words[value%SIZE].docs.printDocs();
-        }
-        else if(words[value%SIZE].next == null){
+        else if (words[value % SIZE].word.compareTo(input) == 0) {
+            output = words[value % SIZE].docs.printDocs();
+        } else if (words[value % SIZE].next == null) {
             output = "Word not found.";
-        }
-        else
-            output = search(words[value%SIZE].next, input);
+        } else
+            output = search(words[value % SIZE].next, input);
 
-        JOptionPane.showMessageDialog(null,input + " was found in the following documents:\n" + output, "Results", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, input + " was found in the following documents:\n" + output, "Results", JOptionPane.PLAIN_MESSAGE);
     }
-    String search(Node inNode, String input){
-        if(inNode.word == null)
+
+    String search(Node inNode, String input) {
+        if (inNode.word == null)
             return "Word not found.";
-        else if(inNode.word.compareTo(input) == 0){
+        else if (inNode.word.compareTo(input) == 0) {
             return inNode.docs.printDocs();
-        }
-        else if(inNode.next == null){
+        } else if (inNode.next == null) {
             return "Word not found.";
-        }
-        else
+        } else
             return search(inNode.next, input);
     }
 
-    int mainHash(String wordIn){
+    int mainHash(String wordIn) {
         int value = 0;
-        for(int j =0; j < wordIn.length(); j++){
+        for (int j = 0; j < wordIn.length(); j++) {
             value += wordIn.charAt(j);
         }
         //value = wordIn.charAt(0)*wordIn.charAt(wordIn.length()-1);
         return value;
     }
-    int stopHash(String wordIn){
-        int value = 0;
-            /*for(int i =0; i < wordIn.length(); i++){
+
+    int stopHash(String wordIn) {
+        /*int value = 0;
+            for(int i =0; i < wordIn.length(); i++){
                 value += wordIn.charAt(i);
             }*/
-        return wordIn.charAt(0)+wordIn.charAt(wordIn.length()-1);
+        return wordIn.charAt(0) + wordIn.charAt(wordIn.length() - 1);
     }
 
     //main function
-    public static void main(String [] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
 
         SearchEngine search = new SearchEngine();
     }
